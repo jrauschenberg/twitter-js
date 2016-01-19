32d54efcac39c2f021342e7e45cbc2d1bdc2968c
John Rauschenberg <john.rauschenberg@gmail.com>
2016-01-19 15:27:27 -0500

    Working basic twitter clone site.

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 var express = require('express');
 var app = express();
 var swig = require('swig');
+var routes = require('./routes/');
 
 swig.setDefaults({cache: false});
 
-app.use(function(req, res, next) {
-  console.log(req.method);
-  console.log(req.route);
-  next();
-})
+app.use('/', routes);
 
-app.use("/special/", function(req, res, next) {
-  console.log("You've reached the special area.");
-  next();
-})
+// app.use(function(req, res, next) {
+//   console.log(req.method);
+//   console.log(req.route);
+//   next();
+// })
 
-app.get("/", function(req, res) {
-  console.log("200");
-  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render('index', {title: 'Hall of Fame', people: people});
-})
+// app.use("/special/", function(req, res, next) {
+//   console.log("You've reached the special area.");
+//   next();
+// })
+
+// app.get("/", function(req, res) {
+//   console.log("200");
+//   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   res.render('index', {title: 'Hall of Fame', people: people});
+// })
+
+app.use(express.static('public'));
 
 app.engine("html", swig.renderFile);
 
